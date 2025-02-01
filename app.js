008a37e8502a6eb737d08fbaaa788a4ea1df00e5
IanRodriguez00 <girm.00@hotmail.com>
2025-02-01 01:19:32 -0600

    validando nombre y agregando amigos al arreglo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+//funcion limpiar caja
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+//Agregar amigo
+function agregarAmigo(){
+    if(!document.querySelector('#amigo').value){
+        alert('¡Nombre invalido!')
+    }else{
+    amigos.push(document.querySelector('#amigo').value)
+    console.log(amigos)
+    limpiarCaja()
+    }
+}
