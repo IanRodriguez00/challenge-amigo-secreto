ca24124daae99fb997b6af2327f339b664ff2c09
IanRodriguez00 <girm.00@hotmail.com>
2025-02-01 01:43:47 -0600

    challenge terminado

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-
 
 //funcion limpiar caja
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 }
 
+//funcion asigna texto a un elemento
+function asignarTextoElemento(elemento, contenido) {
+    const ul = document.querySelector(elemento);
+    ul.innerHTML = ''; 
+    if (Array.isArray(contenido)) {
+        contenido.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = item;
+            ul.appendChild(li);
+        });
+    } else {
+        const li = document.createElement('li');
+        li.textContent = contenido;
+        ul.appendChild(li);
+    }
+}
+
 //Agregar amigo
 function agregarAmigo(){
     if(!document.querySelector('#amigo').value){
-        alert('¡Nombre invalido!')
+        alert('Por favor, ingrese un nombre valido');
     }else{
-    amigos.push(document.querySelector('#amigo').value)
-    console.log(amigos)
-    limpiarCaja()
+        amigos.push(document.querySelector('#amigo').value);
+        console.log(amigos);
+        limpiarCaja();
+        asignarTextoElemento('ul', amigos);
+    }
+}
+
+//Sortear amigo
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert('La lista de amigos está vacía');
+    }else{
+        let numeroAmigo = Math.floor(Math.random() * amigos.length);
+        asignarTextoElemento('ul', `¡Tu amigo secreto es ${amigos[numeroAmigo]}!`);
     }
 }
